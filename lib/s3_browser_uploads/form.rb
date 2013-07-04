9eb8dbad6797ae7cdbdf23ae187863049993f844
Frederick Cheung <frederick.cheung@gmail.com>
2013-07-04 09:08:24 +0100

    store bucket in the new fields hash

--- lib/s3_browser_uploads/form.rb
+++ lib/s3_browser_uploads/form.rb
@@ -1,10 +1,23 @@
 require 'time'
 module S3BrowserUploads
   class Form
-    attr_accessor :region, :aws_access_key_id, :aws_secret_access_key, :aws_session_token, :expires, :bucket
+    attr_accessor :region, :aws_access_key_id, :aws_secret_access_key, :aws_session_token, :expires, :fields
 
- 
+
+    def bucket
+      fields['bucket']
+    end
+
+    def bucket= value
+      add_field 'bucket', value
+    end
+
+    def add_field key, value
+      fields[key] = value
+    end
+    
     def initialize(options={})
+      @fields = {}
       options.each {|key, value| public_send("#{key}=", value)}
     end
  
