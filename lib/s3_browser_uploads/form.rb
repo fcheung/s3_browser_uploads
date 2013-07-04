ed0bf5bcb070b3a6762dc68eeb2e480afa711e16
Frederick Cheung <frederick.cheung@gmail.com>
2013-07-04 08:56:35 +0100

    Start on the policy document

--- lib/s3_browser_uploads/form.rb
+++ lib/s3_browser_uploads/form.rb
@@ -1,3 +1,4 @@
+require 'time'
 module S3BrowserUploads
   class Form
     attr_accessor :region, :aws_access_key_id, :aws_secret_access_key, :aws_session_token, :bucket, :acl, :expires
@@ -8,6 +9,13 @@ module S3BrowserUploads
  
     def endpoint
       "https://#{bucket}.s3-#{region}.amazonaws.com"
+    end
+
+    def policy_document
+      {
+        'expires' => expires.xmlschema,
+        'conditions' => []
+      }
     end
   end
 end
--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe S3BrowserUploads::Form do
+
+  let(:expires_at) {Time.now + 1800}
   [:region, :aws_access_key_id, :aws_secret_access_key, :aws_session_token, :bucket, :acl, :expires].each do |key|
     it "should set #{key} from hash passed to" do
       form = S3BrowserUploads::Form.new(key => 'foo')
@@ -12,6 +14,18 @@ describe S3BrowserUploads::Form do
     it 'should return the url for the bucket' do
       S3BrowserUploads::Form.new(:region => 'eu-west-1', :bucket => 'some-bucket').endpoint.should  ==
         "https://some-bucket.s3-eu-west-1.amazonaws.com"
+    end
+  end
+
+  describe 'policy_document' do
+    let(:form) { S3BrowserUploads::Form.new(:region => 'eu-west-1', :bucket => 'some-bucket', :expires => expires_at)}
+
+    it 'should contain expires and conditions' do
+      form.policy_document.keys.should =~ %w(conditions expires)
+    end
+
+    it 'should set expires to the xml schema representation of the expiry date' do
+      form.policy_document['expires'].should == expires_at.xmlschema
     end
   end
 end
